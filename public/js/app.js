bfc64a30d5d5bbf60458724df8249eaf4f962542
Ahmad Kolagha <ahmadkolagha99@gmail.com>
2022-04-02 19:08:20 +0300

    Setup app for Herohu

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ weatherForm.addEventListener('submit', (e) =>{
     message1.textContent = "loading..."
     message2.textContent = ''
     const location = searchForm.value
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    fetch(`/weather?address=${location}`).then((response) => {
     response.json().then((data) => {
         if(data.error){
             message1.textContent = data.error
