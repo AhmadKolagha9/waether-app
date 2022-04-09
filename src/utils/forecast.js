443b98b418922bf53eb1f4eb98e1cd9556b75070
Ahmad Kolagha <ahmadkolagha99@gmail.com>
2022-04-09 16:54:49 +0300

    add new forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,14 +2,13 @@ const request = require('postman-request')
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=137fc7c569d2af9008d10ffbdebef81f&query=' + encodeURIComponent(latitude)+','+ encodeURIComponent(longitude)
-    request({ url: url, json: true}, (error, response) => {
+    request({ url: url, json: true }, (error, { body }) => {
         if (error) {
-            callback("unable to connect to weather services!", undefined)
-        } else if(response.body.error){
-            callback("unable to find weather. try another search", undefined)
-        } else{
-            callback(undefined, "temperature: " +  response.body.current.temperature +
-            " feelslike: " + response.body.current.feelslike)
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out. It feels like " + body.current.feelslike + " degress out. The humidity is " + body.current.humidity + "%.")
         }
     })
 }
